b56449302f549274829da1f6f79afcb6e12bd01a
tritm97 <minhtripro97@gmail.com>
2024-05-30 16:37:06 +0700

    sửa đổi lần 4

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg|mp3)$/i,
+      test: /\.(mp3)$/i,
       type: "asset",
       generator: {
         filename: "static/chunks/[path][name].[hash][ext]",
